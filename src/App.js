7fce09f0faa55739189472b1276d7a7d90774673
erik hirsch <ehirsch760@gmail.com>
2020-07-09 15:36:58 -0700

    added hasrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import './assets/css/main.css';
 import './assets/css/style.css'
 import LandingPage from "./components/LandingPage"
