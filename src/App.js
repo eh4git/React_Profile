38b81c406aff93c7b1edf8ba1579d2ef640eefa8
erik hirsch <ehirsch760@gmail.com>
2020-07-09 01:26:15 -0700

    fixing io

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default ()=> {
       <Router>
         <div>
           <Switch>
-            <Route exact path="/">
+            <Route exact path="/React_Profile">
               <LandingPage />
             </Route>
             <Route exact path="/profile">
