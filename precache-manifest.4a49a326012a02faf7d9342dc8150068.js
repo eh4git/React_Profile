8e4577b5c5905793a105c9c8629d515c8632117d
erik hirsch <ehirsch760@gmail.com>
2020-07-22 10:40:25 -0700

    Updates

---
+++ precache-manifest.4a49a326012a02faf7d9342dc8150068.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a32581f9aed31d6fb54fbaf9c94d53",
+    "url": "/React_Profile/index.html"
+  },
+  {
+    "revision": "f78f825e6580188f37c3",
+    "url": "/React_Profile/static/css/main.e06c69d4.chunk.css"
+  },
+  {
+    "revision": "27c8e2c838fbc43a8c72",
+    "url": "/React_Profile/static/js/2.edb08e02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Profile/static/js/2.edb08e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78f825e6580188f37c3",
+    "url": "/React_Profile/static/js/main.0c48a99b.chunk.js"
+  },
+  {
+    "revision": "b78cc42073c2204c3cf7",
+    "url": "/React_Profile/static/js/runtime-main.7c74dd80.js"
+  },
+  {
+    "revision": "68f8f684f581d97155f45caed376e03a",
+    "url": "/React_Profile/static/media/WeatherDashboard4.68f8f684.png"
+  },
+  {
+    "revision": "7f8d21336c53383287872278660276b9",
+    "url": "/React_Profile/static/media/WebDevResume.7f8d2133.docx"
+  },
+  {
+    "revision": "a5c13a4ac65d37591f2063816e61563f",
+    "url": "/React_Profile/static/media/WebDevResume.a5c13a4a.pdf"
+  },
+  {
+    "revision": "e3b52d86c827c6a38dafd237a4f21294",
+    "url": "/React_Profile/static/media/WoodPaperBackground.e3b52d86.jpg"
+  },
+  {
+    "revision": "90f7692c0a9cb1ef6f919d6910ea7712",
+    "url": "/React_Profile/static/media/YouMBdApp.90f7692c.png"
+  },
+  {
+    "revision": "c52a43d38b63047dad8a8b36bd369e53",
+    "url": "/React_Profile/static/media/bigbearlakesunset.c52a43d3.jpg"
+  },
+  {
+    "revision": "6a640c4a6b01522001f99b420e40f15a",
+    "url": "/React_Profile/static/media/onceUponAnAdventure.6a640c4a.png"
+  },
+  {
+    "revision": "22db85056377d7bab6cc3312cd68c25d",
+    "url": "/React_Profile/static/media/stars-forest-silhouette.22db8505.gif"
+  },
+  {
+    "revision": "769453e79bbec4631006256df6231fa0",
+    "url": "/React_Profile/static/media/storyTime.769453e7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac9676498ebf4652753751e2cf21fb5e.js"
+  "/React_Profile/precache-manifest.4a49a326012a02faf7d9342dc8150068.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_Profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
